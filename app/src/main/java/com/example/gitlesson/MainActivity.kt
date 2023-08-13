be94024ee55d397fb92ed15b19954bf44b543244
TeraIter <forensic168@gmail.com>
2023-08-14 01:30:25 +0500

    Development branch

--- app/src/main/java/com/example/gitlesson/MainActivity.kt
+++ app/src/main/java/com/example/gitlesson/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.gitlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println(this is sixth commit)
+        val string = "Hello World"
+        Log.d("string", string)
     }
 }
