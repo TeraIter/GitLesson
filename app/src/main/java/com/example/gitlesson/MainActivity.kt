7a6c0bd132e1d8cd7cb46e86005406679cfe94d2
TeraIter <forensic168@gmail.com>
2023-08-13 22:23:18 +0500

    second commit

--- app/src/main/java/com/example/gitlesson/MainActivity.kt
+++ app/src/main/java/com/example/gitlesson/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.gitlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val x = 1
+        Log.d("hi", "hello")
+        println("hello world")
     }
 }
